30a2f3435cc4d0bf100a4e7ac9f0e428e79d2553
Kadies Vu <kadiesvu@Kadiess-iMac.local>
2019-08-18 04:45:32 +0700

    UI

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -4,17 +4,13 @@ import { NavController } from 'ionic-angular';
 import { SignupPage } from '../signup/signup';
 import { ForgotpasswordPage } from '../forgotpassword/forgotpassword';
 import { TabsPage } from '../tabs/tabs';
-import {Server} from '../../providers/server/server';
-import {Account} from '../../providers/server/account';
 @Component({
   selector: 'page-signin',
   templateUrl: 'signin.html'
 })
 export class SigninPage {
- public id:string;
-  public pw:string;
-  public error_login:string;
-  constructor(public myServer:Server,public account:Account,public navCtrl: NavController) {
+
+  constructor(public navCtrl: NavController) {
 
   }
      
@@ -27,24 +23,5 @@ export class SigninPage {
  tabs(){
         this.navCtrl.setRoot(TabsPage)
   } 
-login(){
-    console.log("vao day cai ha");
-    let postData = {
-      "id": this.id,
-      "pw": this.pw
-  }
-  this.myServer.sendRequest("Login/Loginin",postData,(data)=>{
-  var stt = data["stt"];
-  if(stt == 1)
-  {
-    console.log("login thành công");
-    this.account.SaveDataLogin(data);
-    this.tabs();
-  }
-  else{
-    this.error_login = data["msg"];
-  }
 
-  });
-  }
 }
