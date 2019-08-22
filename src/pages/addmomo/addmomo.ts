fe1c24cf10d1ae4ef9dbca3b87c91e4cfc48e7ac
nguyensitien <nguyensitien481994@gmail.com>
2019-08-23 00:06:17 +0700

    Qr Momo

--- src/pages/addmomo/addmomo.ts
+++ src/pages/addmomo/addmomo.ts
@@ -2,7 +2,6 @@
 
 
 
-import { Component } from '@angular/core';
 import { NavController, ModalController} from 'ionic-angular';
 
 import { PromocodePage } from '../promocode/promocode';
@@ -11,6 +10,7 @@ import { AlertController } from 'ionic-angular';
 import { ToastController } from 'ionic-angular';
 import {Events} from 'ionic-angular';
 import {Account} from '../../providers/server/account';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 @Component({
   selector: 'page-addmomo',
   templateUrl: 'addmomo.html'
@@ -27,11 +27,12 @@ export class AddMomoPage {
   public chietkhau_vtts:string;
   public chietkhau_ftth:string;
   public chietkhau_mb:string;
-
+  public qr_url:string;
+  public code:string;
   constructor(public account:Account,public events:Events,private toast: ToastController,private alertCtrl: AlertController,public myServer:Server,public navCtrl: NavController, public modalCtrl: ModalController) {
     this.sotienTmp = 0;
-   
-    
+    this.qr_url = "https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=12c";
+    this.code = "";
   }
 
 
@@ -72,7 +73,10 @@ export class AddMomoPage {
     this.myServer.getRequest("Login/momo_regCharge",postData,(data)=>{
         var stt = data["stt"];
         if(stt == 1){
-          this.account.UpdateSotien(data["data"]["st"]);
+          data = data["data"];
+          this.account.UpdateSotien(data["st"]);
+          this.qr_url = data["qr_url"];
+          this.code = data["code"];
           this.events.publish('updateScreen');
           this.presentToast("Nạp Tiền Thành Công!");
           
@@ -86,5 +90,21 @@ export class AddMomoPage {
     let modal = this.modalCtrl.create(PromocodePage);
     modal.present();
   }
+  copyCode(item)
+  {
+    document.addEventListener('copy', (e: ClipboardEvent) => {
+      e.clipboardData.setData('text/plain', (item));
+      this.presentToast("Copy thành công!");
+      e.preventDefault();
+      document.removeEventListener('copy', null);
+    });
+    document.execCommand('copy');
+   
+  }
+
+  openMomo(){
+    
+  }
+
 
 }
